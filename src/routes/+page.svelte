35eaed2d489328a82663796898fd556bf381bea4
Joseph Rollins <joseph@guilded.gg>
2023-03-29 09:21:31 -0700

    Remove prettier, add basic grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let cells = Array.from({ length: 25 });
+</script>
+
+<h1>Bingo</h1>
+
+<div class="container">
+  {#each cells as _}
+    <div class="cell" />
+  {/each}
+</div>
+
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: repeat(5, auto);
+    grid-template-rows: repeat(5, auto);
+    width: 500px;
+    height: 500px;
+    grid-gap: 10px;
+  }
+
+  .cell {
+    border: 1px solid blue;
+    border-radius: 5px;
+  }
+  .cell:before {
+    content: '';
+    display: block;
+  }
+</style>
