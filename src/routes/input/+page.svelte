4ac0ecfddbbc63a7932f3dab604febbaa2531ff5
Joseph Rollins <rollins.joseph@gmail.com>
2023-04-01 16:51:03 -0700

    Work on autocomplete input

--- src/routes/input/+page.svelte
+++ src/routes/input/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { debounce } from "lodash";
+  import fallbackImg from "$lib/images/missing_cover.jpg";
 
   let search = "";
   let results: any = [];
@@ -7,6 +8,7 @@
   const handleChange = debounce(async () => {
     const term = search;
     if (term.length < 3) {
+      results = [];
       return;
     }
     const json = await fetch(
@@ -19,11 +21,72 @@
 </script>
 
 <h1>Input</h1>
-<input bind:value={search} on:input={handleChange} />
-<ul>
-  {#each results as result}
-    <li>
-      {result.volumeInfo.title}
-    </li>
-  {/each}
-</ul>
+<div class="container">
+  <input bind:value={search} on:input={handleChange} />
+  <ul>
+    {#each results as result}
+      <li>
+        <img
+          class="cover"
+          src={result.volumeInfo.imageLinks?.smallThumbnail || fallbackImg}
+          alt="Book cover art"
+        />
+        <div>
+          <div class="title">
+            {result.volumeInfo.title}
+          </div>
+          <div class="author">
+            {result.volumeInfo.authors?.join(", ") || ""}
+          </div>
+        </div>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .container {
+    width: 320px;
+    display: flex;
+    flex-direction: column;
+  }
+  input {
+    flex: 1;
+    padding: 8px 12px;
+    border-radius: 4px;
+    border: 1px solid #d1d1d1;
+    background-color: white;
+    font-size: 18px;
+  }
+  ul {
+    flex: 1;
+    padding: 0;
+    margin: 0;
+    border: 1px solid #d1d1d1;
+    border-top: none;
+    background-color: white;
+    max-height: 320px;
+    overflow: scroll;
+  }
+  li {
+    padding: 4px;
+    list-style-type: none;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+  li:hover {
+    background-color: #f5f5f5;
+  }
+  .cover {
+    max-height: 60px;
+    width: 42px;
+  }
+  .title {
+    font-size: 18px;
+  }
+  .author {
+    font-size: 12px;
+    color: #232323;
+  }
+</style>
